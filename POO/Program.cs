b644f780c0751d316b9950318add0211eea2066c
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-02-20 15:31:55 -0300

    Mao na massa - Trabalhando com Listas

--- POO/Program.cs
+++ POO/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using POO.ContentContext;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace POO
 {
@@ -13,11 +14,38 @@ namespace POO
             articles.Add(new Article("Artigo sobre C#", "cshap"));
             articles.Add(new Article("Artigo sobre .NET", "dotnet"));
 
-            foreach (var article in articles)
+            // foreach (var article in articles)
+            // {
+            //     Console.WriteLine(article.Id);
+            //     Console.WriteLine(article.Title);
+            //     Console.WriteLine(article.Url);
+            // }
+
+            var courses = new List<Course>();
+            var courseOOP = new Course("Fundamentos OOP", "faundamentos-oop");
+            var courseCsharp = new Course("Fundamentos C#", "faundamentos-cshap");
+            var courseAspNet = new Course("Fundamentos ASP.NET", "faundamentos-aspnet");
+            courses.Add(courseOOP);
+            courses.Add(courseCsharp);
+            courses.Add(courseAspNet);
+
+            var careers = new List<Career>();
+            var careerDotnet = new Career("Especialista .NET", "especialista-dotnet");
+            var careerItem2 = new CareerItem(2, "Aprenda OOP", "", null);
+            var careerItem = new CareerItem(1, "Comece por aqui", "", courseCsharp);
+            var careerItem3 = new CareerItem(3, "Aprenda .NET", "", courseAspNet);
+            careerDotnet.Items.Add(careerItem2);
+            careerDotnet.Items.Add(careerItem3);
+            careerDotnet.Items.Add(careerItem);
+            careers.Add(careerDotnet);
+
+            foreach (var career in careers)
             {
-                Console.WriteLine(article.Id);
-                Console.WriteLine(article.Title);
-                Console.WriteLine(article.Url);
+                Console.WriteLine(career.Title);
+                foreach (var item in career.Items.OrderBy(x => x.Order))
+                {
+                    Console.WriteLine($"{item.Order} - {item.Title}"); //composicao de strings
+                }
             }
         }
     }
